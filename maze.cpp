82bad70c6ecabed5675a9987f89d94582629a3e3
Alexander Tromp PC <alexandert@live.nl>
2019-10-13 23:01:06 +0200

    100% maze.cpp

--- maze.cpp
+++ maze.cpp
@@ -12,8 +12,7 @@ class position {
         int x, y;
 
     public:
-        //position();
-        position(int x1, int y1) {                                                       //constructor position that sets x and y coordinates
+        position(int x1, int y1) {
             x = x1; 
             y = y1;
         }
@@ -26,30 +25,39 @@ class maze {
         vector<string> line;
 
     public:
-        //maze();
-        maze(vector<string> lines) {                                                     //constructor maze that sets the current vector to the vector given as input in the argument
+        maze(vector<string> lines) { 
             line = lines;
         }
-        bool isWall(position& coordinates) {                                                     //method isWall returning true or false 
-            cout << line.at(coordinates.getX());            //REMOVE
-            return line.at(coordinates.getX()).at(coordinates.getY()) == 'l';
+        bool isWall(position playercoordinates) { 
+            return (line.at(playercoordinates.getX()).at(playercoordinates.getY()) == 'l');   
         }
-        bool isWithinBounds(position& coordinates) {                                             //method isWithinBounds returning true or false
-            return coordinates.getX() >= 0 && coordinates.getX() < line.size()
-            && coordinates.getY() >= 0 && coordinates.getY() < line.at(coordinates.getX()).length();
+        void validateExit(position exitcoordinates) {
+            if (line.at(exitcoordinates.getX()).at(exitcoordinates.getY()) == 'l') {
+                throw runtime_error("an error occurred: exit outside maze or off the path");
+            }
         }
-        void print(position& playerPosition) {                                                          //method print with input player position
-            cout << "Print called";                         //REMOVE
-            for (int i = 0; i < line.size(); ++i) {
-                for (int j = 0; i < line.at(i).length(); ++j) {
+        void print(position playerPosition, position endPosition, bool calculation) { 
+            for (int i = 0; i < line.size(); i++) {
+                for (int j = 0; j < line.at(i).length(); j++) {
                     if (i == playerPosition.getX() && j == playerPosition.getY()) {
-                        cout << 'P';
+                        cout << "P";
+                    }
+                    else if (i == endPosition.getX() && j == endPosition.getY()) {
+                        cout << "x";
                     }
                     else {
-                        cout << line.at(i).at(j);
+                    cout << line.at(i).at(j);
                     }
                 }
                 cout << endl;
+            }
+            if (calculation == true) {
+                if (playerPosition.getX() == endPosition.getX() && playerPosition.getY() == endPosition.getY()) {
+                    cout << endl << "The player reached the exit!" << endl;
+                }
+                else {
+                    cout << endl << "The player did not reach the exit." << endl;
+                }
             }
         }
 };
@@ -60,63 +68,54 @@ class gameState {
     
     public:
         gameState(maze maze1) : mymaze(maze1) {}
-        /*gameState(maze& maze1) {
-            mymaze = maze1;
-        }*/
-       
-        
-        position performMoves(string moves, position& coordinates) {
-            cout << "performMoves" << endl;
+
+        position performMoves(string moves, position coordinates) {
             for (int i = 0; i < moves.length(); ++i) {
-                cout << moves.at(i) << endl;
                 coordinates = performMove(moves.at(i), coordinates);
             }
             return coordinates;
         }
-        position performMove(char move, position& coordinates) {
-            //position nextPosition;
-            
-            switch (move)
-            {
-            case 'u':
-                cout << "U";
-                coordinates = position(coordinates.getX() - 1, coordinates.getY());
-                break;
-            case 'r':
-                cout << "R";
-                coordinates = position(coordinates.getX(), coordinates.getY() + 1);
-                break;
-            case 'd':
-                cout << "D";
-                coordinates = position(coordinates.getX() + 1, coordinates.getY());
-                break;
-            case 'l':
-                cout << "L";
-                coordinates = position(coordinates.getX(), coordinates.getY() - 1);
-                break;
-            default:
-                throw runtime_error("an error occurred: invalid move");
-                break;
-            }
-            if (mymaze.isWall(coordinates)) {
-                throw runtime_error("an error occured: player outside maze or off the path");
-            }
-            if (!mymaze.isWithinBounds(coordinates)) {
-                throw runtime_error("an erro occured: player outside maze or off the path");
-            }
+        position performMove(char move, position coordinates) {
+            switch (move) {
+                case 'u':
+                    coordinates = position(coordinates.getX() - 1, coordinates.getY());
+                    break;
+                case 'r':
+                    coordinates = position(coordinates.getX(), coordinates.getY() + 1);
+                    break;
+                case 'd':
+                    coordinates = position(coordinates.getX() + 1, coordinates.getY());
+                    break;
+                case 'l':
+                    coordinates = position(coordinates.getX(), coordinates.getY() - 1);
+                    break;
+                default:
+                    throw runtime_error("an error occurred: invalid move");
+                    break;
+            } 
+            //if (mymaze.isWall(coordinates)) {
+                //throw runtime_error("blablabla");
+            //}
             return coordinates;
         }
 };
 
-
-//Parsing input from a string to integers
-void stringToInteger(string line, int& first, int& second) {
+//Checks if both coordinates are integers
+void checkCoordinates(string line, int& first, int& second) {
     stringstream ss;
     ss << line;
     ss >> first >> second;
     if (ss.fail()) {
-        throw runtime_error("an error occured: could not read height and width of the maze");
+        throw runtime_error("an error occurred: could not read coordinates");
         ss.clear();
+    }
+}
+
+//Checks if height and width are valid
+void checkDimensions(string line, int& first, int& second) {
+    istringstream iss(line);
+    if (!(iss >> first >> second)) {
+        throw runtime_error("an error occurred: could not read height and width of the maze");
     }
 }
 
@@ -127,53 +126,57 @@ int main(int argc, char* argv[]) {
             inFile = argv[1];
         }
         else {
-            throw runtime_error("an error occured: no input file name given \n");
+            throw runtime_error("an error occurred: no input file name given");
         }
     
         ifstream inputFile;
         inputFile.open(argv[1]);
         if (!inputFile.is_open()) {
-            string error = "an error occured: could not open input file ";
+            string error = "an error occurred: could not open input file ";
             error.append(argv[1]);
-            throw runtime_error(error);
-            
+            throw runtime_error(error); 
         }
-        //File is now open
 
         string line;
-
-        //Width and height of the maze
         getline(inputFile, line);
         int height, width;
-        stringToInteger(line, height, width);
+        checkDimensions(line, height, width);
 
         vector<string> lines;
         for (int i = 0; i < height; ++i) {
             getline(inputFile, line);
-            lines.push_back(line);
+            if (line.length() == width) {
+                lines.push_back(line);
+            }
+        }
+
+        if (lines.size() != height) {
+            throw runtime_error ("an error occurred: could not read maze layout");
         }
 
         int x, y;   
         getline(inputFile, line);
-        stringToInteger(line, x, y);
-        position start(x, y);                   //Starting position 
+        checkCoordinates(line, x, y);
+        position end(x, y); 
         getline(inputFile, line);
-        stringToInteger(line, x, y);
-        position end(x, y);                     //Ending position 
-        string moves;                           //Player moves
+        checkCoordinates(line, x, y);
+        position start(x, y);
+        string moves;
         getline(inputFile, moves);
         
-
-        maze maze(lines);                       //Creating object maze for class maze with vector<string> as input
-        gameState state(maze);                  //Creating object state for class gameState with maze as input
-        maze.print(start);
-
-        cout << endl;
-        
+        maze maze(lines);
+        maze.validateExit(end);
+        gameState state(maze); 
         gameState GameState = gameState(maze);
         position result = GameState.performMoves(moves, start);
-        maze.print(result);
-        
+
+        maze.print(start, end, false);
+        cout << endl;
+        if (maze.isWall(result)) {
+            throw runtime_error("an error occurred: player outside maze or off the path");
+        }
+        maze.print(result, end, true);
+
         inputFile.close();
     }
 
--- test.cpp
+++ test.cpp
@@ -1,21 +1,41 @@
+#include <sstream>
+#include <string>
 #include<iostream>
+#include<fstream>
 #include<string>
+#include<sstream>
+#include<vector>
+#include<exception>
+#include<limits>
 
 using namespace std;
 
-int main() {
-    string input;
-    int i;
 
-    cin >> input;
-    cout << "command: " << input << endl;
-    cin >> input; 
-    i = stoi(input);
-    cout << "argument: " << i << endl;
+int main(int argc, char* argv[]) {
+        string inFile = "";
+        if (argc == 2) {
+            inFile = argv[1];
+        }
+        else {
+            throw runtime_error("an error occurred: no input file name given");
+        }
+    
+        ifstream inputFile;
+        inputFile.open(argv[1]);
+        if (!inputFile.is_open()) {
+            string error = "an error occurred: could not open input file ";
+            error.append(argv[1]);
+            throw runtime_error(error);
+            
+        }
 
 
 
+    string line;
+    getline(inputFile, line);
+    istringstream iss(line);
+    int a, b;
+    if (!(iss >> a >> b)) { cout << "error" << endl; } // error
 
-
-    return 0;
+    cout << a << " " << b << endl;
 }
